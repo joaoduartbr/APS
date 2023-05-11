c6fb5637f6142298ce7ebf7b41c565d1ff3e4eba
José Carlos <josecarloscardosobrito@gmail.com>
2023-05-11 17:23:15 -0300

    cerradex

--- js/index.js
+++ js/index.js
@@ -30,48 +30,69 @@
          board.classList.add("dark")
 
          document.querySelector("body").style.backgroundColor="rgb(21, 23, 30)"
+         document.querySelector("footer").style.backgroundColor="white"
+         document.querySelector("footer").style.color="black"
      }else{
          e.target.innerText = "🌙"
          board.classList.remove("dark")
          board.classList.add("light"); 
          document.querySelector("body").style.backgroundColor="white"
+         document.querySelector("footer").style.backgroundColor="rgb(21, 23, 30)"
+         document.querySelector("footer").style.color="white"
      }
      
      
  });
  selectPage(true)
 
-function setCerradex(){
-// const endpoint = 'https://api.gbif.org/v1/species/search';
-// const params = {
-//   kingdom: 'Animalia',
-//   rank: 'SPECIES',
-//   // adicione seu token de acesso aqui, se necessário
-// };
-// const url = new URL(endpoint);
-// url.search = new URLSearchParams(params).toString();
-// // console.log(url)
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data)
-//     fetch('https://api.unsplash.com/search/photos' + '?query=' + data.results[0].canonicalNmame, {
-//         headers: {
-//             Authorization: 'Client-ID ' + "Uav8XoQ_N1t2FeIrRglbTPAK6X0m-3KZethJ7TnNhRI"
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         // Loop through the search results and display the images on the webpage
-//         let imagesHtml = '';
-//         data.results.forEach(function(result) {
-//             imagesHtml += '<img src="' + result.urls.regular + '" alt="' + result.alt_description + '">';
-//         });
-//         document.getElementById('image-grid').innerHTML = imagesHtml;
-//     })
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
 
+let dado =null;
+function setCerradex(){
+ fetch("../public/cerradex.json").then(data=>data.json( )).then(data=>{
+     dado=data
+    data.forEach((e,id)=>{
+        const block = document.createElement("div")
+        block.id = `${id}`
+        block.classList.add("selectBlock")
+
+        const span = document.createElement("span")
+        span.innerText = `#${id}`
+        
+        const img = document.createElement("img")
+        img.src=e.img
+
+        const name = document.createElement("p")
+        name.innerText = e.species
+
+        block.appendChild(span)
+        block.appendChild(img)
+        block.appendChild(name)
+
+        block.addEventListener("click",showStatus)
+        document.querySelector("#image-grid").appendChild(block)
+    })
+ })
+
+}
+
+function showStatus(e){
+    const id = Number(e.target.id)
+    const especie = dado[id]
+    document.querySelector(".board").innerHTML=`
+    <div id="statusContainer">
+    <div><h1>${especie.species}</h1> <span>#${id}</span></div>
+    <img src="${especie.img}">
+    <div id="infos">
+        <div id="family">
+       <span>Familia</span> <p>${especie.family}</p>
+        </div>
+        <div id="sName">
+       <span>Nome cientifico</span> <p>${especie.scientific_name}</p>
+        </div>
+        <div id="status">
+       <span>status</span> <p>${especie.status}</p>
+        </div>
+    </div>
+    </div>
+    `
 }
